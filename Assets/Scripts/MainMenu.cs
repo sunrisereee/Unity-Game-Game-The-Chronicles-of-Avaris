363259cd9a7eb3c5e0ceda9df4ce98738c59729f
sunrisereee <gkarpeykin@mail.ru>
2024-12-14 10:56:06 +0300

    fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,23 +6,12 @@ public class MainMenu : MonoBehaviour
     {
         //подт€гиваем следующую сцену после меню 
         Debug.Log("Start game...");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void ExitGame()
     {
         // выкидываем нашу игру в урну
         Debug.Log("Closing game...");
         Application.Quit();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
